9d3bd052eed334fab784a33468399904df6cbdeb
Justin <JMariner@ecpi.edu>
2022-11-29 19:55:52 -0500

    Fix env loading

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const { config } = require("dotenv");
-const startServer = require("./lib/server");
 
 config({ path: path.resolve(__dirname, ".env") });
 
+const startServer = require("./lib/server");
 startServer();
